225fa561cbe237ad29f8df0db2ccf6fb31f2c682
sas1711069 <104480842+sas1711069@users.noreply.github.com>
2022-06-06 12:23:22 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,26 @@
+music.setVolume(127)
+basic.forever(function () {
+    serial.writeLine("" + (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )))
+    basic.showNumber(sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ))
+})
+basic.forever(function () {
+    music.playTone(294, music.beat(BeatFraction.Half))
+    basic.pause(200)
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    basic.pause(200)
+    music.playTone(784, music.beat(BeatFraction.Half))
+    basic.pause(200)
+    music.playTone(784, music.beat(BeatFraction.Half))
+    basic.pause(200)
+    music.playTone(880, music.beat(BeatFraction.Half))
+    basic.pause(200)
+    music.playTone(880, music.beat(BeatFraction.Half))
+})
